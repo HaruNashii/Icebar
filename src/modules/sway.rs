15684d69365843bb47f121b7fa80893d9883cf3d
Elizabeth <parajogapaladins@gmail.com>
2026-02-25 18:47:31 -0300

    fix SwayWorkspaces wrapping around when persistent_workspaces was enable

--- src/modules/sway.rs
+++ src/modules/sway.rs
@@ -56,22 +56,50 @@ pub fn workspace_count() -> Vec<i32>
 pub fn change_workspace_sway(action: UserSwayAction)
 {
     let result_conn = Connection::new();
-    if let Ok(mut conn) = result_conn
+    match result_conn
     {
-        match action 
+        Ok(mut conn) =>
         {
-            UserSwayAction::ChangeWithIndex(index) =>
+            match action 
             {
-                let _ = conn.run_command(format!("workspace number {index}"));
-            }
-            UserSwayAction::MoveNext =>
-            {
-                let _ = conn.run_command("workspace next");
-            }
-            UserSwayAction::MovePrev =>
-            {
-                let _ = conn.run_command("workspace prev");
+                UserSwayAction::ChangeWithIndex(index) =>
+                {
+                    let output = conn.run_command(format!("workspace number {index}"));
+                    if let Err(err) = output { println!("Warning!!! Couldn't Switch Workspaces With Index Using SwayIPC\nErr: {err}") }
+                }
+                UserSwayAction::MoveNext =>
+                {
+                    let result_workspaces = conn.get_workspaces();
+                    if let Ok(workspaces) = result_workspaces
+                    {
+                        for workspace in workspaces
+                        {
+                            if workspace.focused
+                            {
+                                let output = conn.run_command(format!("workspace number {}", workspace.num + 1)).map_err(|e| e.to_string());
+                                if let Err(err) = output { println!("Warning!!! Couldn't Switch To The Next Workspaces With SwayIPC\nErr: {err}") };
+                            }
+                        }
+                    }
+                }
+                UserSwayAction::MovePrev =>
+                {
+                    let result_workspaces = conn.get_workspaces();
+                    if let Ok(workspaces) = result_workspaces
+                    {
+                        for workspace in workspaces
+                        {
+                            if workspace.focused
+                            {
+                                let workspace_to_parse = if workspace.num - 1 <= 0 { 1 } else { workspace.num - 1 };
+                                let output = conn.run_command(format!("workspace number {}", workspace_to_parse)).map_err(|e| e.to_string());
+                                if let Err(err) = output { println!("Warning!!! Couldn't Switch To The Previous Workspaces WIth SwayIPC\nErr: {err}") };
+                            }
+                        }
+                    }
+                }
             }
         }
+        Err(err) => println!("\n\n\nFailed To Connect With The SwayIPC!!!\nErr: {err}\n\n\n")
     }
 }
